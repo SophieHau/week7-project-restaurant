6e4a87e78ee790792fe919fc00e1dcffe02b5cf1
Sophie <you@example.com>
2019-03-27 21:45:55 +0200

    Creating menu, order and (partial) leave apps

--- app.py
+++ app.py
@@ -26,12 +26,16 @@ class Table(db.Model):
     Its fields should include an indication whether the table is occupied
     or not.'''
     # To do: table definition (class properties)
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.Text)
 
     @classmethod
     def get_free_table(cls):
         '''Get the first found Table which is unoccupied (free).'''
         # To do: method definition
-        pass
+        free_table = Table.query.filter_by(status='free').first()
+        return free_table
+
 
 
 class MenuItem(db.Model):
@@ -39,7 +43,13 @@ class MenuItem(db.Model):
     Its fields should include a name, price, image, and any others you think
     are relevant.'''
     # To do: table definition (class properties)
-    pass
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    price = db.Column(db.Integer)
+    image = db.Column(db.Text)
+    category = db.Column(db.Text)
+
 
 
 class Order(db.Model):
@@ -65,7 +75,18 @@ def customer_index():
     Then redirect to /table/<id>, where <id> is the id of the found table.
     If not, render the index page, with a message saying that all the
     tables are full, etc.'''
-    pass
+    table_id = get_session_table_id()
+    if table_id != None:
+        return redirect(url_for('show_table', table_id=table_id))
+    else:
+        free_table = Table.get_free_table()
+        free_table.status ='occupied'
+        db.session.add(free_table)
+        db.session.commit()
+        set_session_table_id(free_table.id)
+        table_id = get_session_table_id()
+        return redirect(url_for('show_table', table_id=table_id))
+    return render_template('customer/index.html')
 
 
 @app.route('/table/<int:table_id>/')
@@ -87,7 +108,18 @@ def show_table(table_id):
     Also show, beneath the menu, a list of the orders already made for
     the table.
     Also display a link/button to leave the table.'''
-    pass
+    session_table_id = get_session_table_id()
+    table = Table.query.get(table_id)
+    if session_table_id != table_id:
+        flash('This is not your table!')
+        return redirect(url_for('customer_index'))
+    elif table.status == 'free':
+        set_session_table_id(None)
+        return redirect(url_for('customer_index'))
+
+    menu = MenuItem.query.all()
+    orders = Order.query.filter_by(table_id=table_id).all()
+    return render_template('customer/table.html', menu=menu, orders=orders)
 
 
 @app.route('/table/<int:table_id>/leave/')
@@ -98,13 +130,15 @@ def leave_table(table_id):
     Delete all orders for the table.
 `   Clear the session table id.
     Render the 'bye' template.'''
-    pass
+    Order.query.filter_by(table_id=table_id).delete()
+    db.session.commit()
+    set_session_table_id(None)
+    return render_template('customer/bye.html')
 
 
 @app.route('/order/<int:menu_item_id>/')
 def make_order(menu_item_id):
     '''Place an order for the given menu item, for the current table.
-
     To do:
     Validation:
      - if the session table is None, redirect to the customer index page.
@@ -113,13 +147,27 @@ def make_order(menu_item_id):
        page.
      - if there is no menu item matching menu_item_id, redirec to the
        show table page.
-
     Create an Order object with the menu item and the table as arguments
     (use the objects, not their ids).
     Save this new Order object to the database.
     Add a flash message telling the customer that their order has been placed.
     Redirect to the show table page.'''
-    pass
+    session_table_id = get_session_table_id()
+    table = Table.query.get(session_table_id)
+    table_id = table.id
+    menu_item = MenuItem.query.get(menu_item_id)
+    if session_table_id == None:
+        return redirect(url_for('customer_index'))
+    elif table == None or table.status is 'free':
+        return redirect(url_for('customer_index'))
+    elif menu_item == None:
+        return redirect(url_for('show_table', table_id=table_id))
+
+    order = Order(menu_item=menu_item, table=table)
+    db.session.add(order)
+    db.session.commit()
+    flash('Your item has been added to your order')
+    return redirect(url_for('show_table', table_id=table_id))
 
 
 @app.route('/kitchen/')
--- seed.py
+++ seed.py
@@ -1,19 +1,35 @@
-# from app import db
-# from app import Table, MenuItem
+from app import db
+from app import Table, MenuItem
 
 
 # db.create_all()
 
 
-# def create_tables():
-#     '''Create 10 Table objects, and commit to database.'''
-#     pass
+def create_tables():
+    '''Create 10 Table objects, and commit to database.'''
+    tables = []
+    for i in range(1,11):
+        table = 'table{}'.format(i)
+        tables.append(table)
+    for table in tables:
+        table = Table()
+        db.session.add(table)
+    db.session.commit()
 
 
-# def create_menu_items():
-#     '''Create a few menu items.'''
-#     pass
+def create_menu_items():
+    '''Create a few menu items.'''
+    menu_item1 = MenuItem('Pizza', 35, 'https://api-content.prod.pizzahutaustralia.com.au//umbraco/api/Image/Get2?path=assets/products/menu/Meat-Super-Supreme-Pizza-3250-menu.jpg')
+    menu_item2 = MenuItem('Pasta', 40, 'https://thebusybaker.ca/wp-content/uploads/2018/08/easy-15-minute-pasta-carbonara-fbig1.jpg')
+    menu_item3 = MenuItem('Salad', 25, 'https://www.halfbakedharvest.com/wp-content/uploads/2018/01/Rejuvenating-Winter-Broccoli-Salad-1-500x500.jpg')
+    menu_item4 = MenuItem('Burger', 30, 'https://www.kitchensanctuary.com/wp-content/uploads/2018/03/Asian-Sticky-Belly-Pork-Burger-with-Pickled-Vegetables-recipe-square-FS.jpg')
+    menu_item5 = MenuItem('Coffee', 15, 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/04/15/16/china-coffee-cup.jpg?w968h681')
+    menu_item6 = MenuItem('Tea', 10, 'https://oxygenyogaandfitness.com/wp-content/uploads/2015/02/brown-brewed-tea.jpg')
+    menu_item7 = MenuItem('Beer', 20, 'https://media-cdn.tripadvisor.com/media/photo-s/0d/a2/a8/ec/cold-beer.jpg')
+    menu_items = [menu_item1, menu_item2, menu_item3, menu_item4, menu_item5, menu_item6, menu_item7]
+    for menu_item in menu_items:
+        db.session.add(menu_item)
+    db.session.commit()
 
-
-# create_tables()
+create_tables()
 # create_menu_items()
